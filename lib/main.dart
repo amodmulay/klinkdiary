9bd38b6efa75719d5540c55f14740b60789118ba
Amod Mulay <amod.mulay@gmail.com>
2022-02-18 11:16:10 +0100

    added Theme. Seperated the login page from the main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:klinikdiary/pages/BodyTempaturePage.dart';
 import 'package:klinikdiary/pages/Homepage.dart';
 import 'package:klinikdiary/pages/Pages.dart';
+import 'theme/klinik_theme.dart';
+import 'package:klinikdiary/pages/login_page.dart';
 
 void main() {
   runApp(const Klinikdiary());
@@ -13,125 +15,19 @@ class Klinikdiary extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+
+    final theme = KlinikTheme.dark();
+
     return MaterialApp(
+
       title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      initialRoute: PAGE_LOGIN,
+      theme: theme,
+      initialRoute: PAGE_HOME,
       routes: {
-        PAGE_LOGIN : (context) => const LandingPage(title: "Klinik Diary"),
+        PAGE_LOGIN : (context) => const LoginPage(),
         PAGE_HOME: (context) => const Homepage(),
         PAGE_BODY_TEMPERATURE: (context) => const BodyTemperaturePage()
       }
-    );
-  }
-}
-
-class LandingPage extends StatefulWidget {
-  const LandingPage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<LandingPage> createState() => _LandingPageState();
-}
-
-class _LandingPageState extends State<LandingPage> {
-  TextStyle style = const TextStyle(fontFamily: 'Montserrat', fontSize: 20.0);
-
-  @override
-  Widget build(BuildContext context) {
-
-    final emailField = TextField(
-      obscureText: false,
-      style: style,
-      decoration: InputDecoration(
-          contentPadding: const EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
-          hintText: "Email",
-          border:
-          OutlineInputBorder(borderRadius: BorderRadius.circular(32.0))),
-    );
-    final passwordField = TextField(
-      obscureText: true,
-      style: style,
-      decoration: InputDecoration(
-          contentPadding: const EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
-          hintText: "Password",
-          border:
-          OutlineInputBorder(borderRadius: BorderRadius.circular(32.0))),
-    );
-    final loginButton = Material(
-      elevation: 5.0,
-      borderRadius: BorderRadius.circular(30.0),
-      color: const Color(0xff01A0C7),
-      child: MaterialButton(
-        minWidth: MediaQuery.of(context).size.width,
-        padding: const EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
-        onPressed: () {
-          Navigator.pushNamed(context, PAGE_HOME);
-        },
-        child: Text("Login",
-            textAlign: TextAlign.center,
-            style: style.copyWith(
-                color: Colors.white, fontWeight: FontWeight.bold)),
-      ),
-    );
-
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Welcome to Klinik Diary"),
-      ),
-      body: Center(
-        child: Container(
-          color: Colors.white,
-          child: Padding(
-            padding: const EdgeInsets.all(36.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                SizedBox(
-                  height: 155.0,
-                  child: Image.asset(
-                    "assets/firebase.jpeg",
-                    fit: BoxFit.contain,
-                  ),
-                ),
-                const SizedBox(height: 45.0),
-                emailField,
-                const SizedBox(height: 25.0),
-                passwordField,
-                const SizedBox(
-                  height: 35.0,
-                ),
-                loginButton,
-                const SizedBox(
-                  height: 15.0,
-                ),
-              ],
-            ),
-          ),
-        ),
-      ),
     );
   }
 }
