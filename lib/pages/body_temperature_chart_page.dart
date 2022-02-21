c1d678935b6cc2483e1fa843c67369c8f794658d
Christian Felbermair <christian.felbermair@gmx.de>
2022-02-21 16:55:40 +0100

    * fixed overflow at body temperature chart page

--- lib/pages/body_temperature_chart_page.dart
+++ lib/pages/body_temperature_chart_page.dart
@@ -2,13 +2,11 @@ import 'package:flutter/material.dart';
 import '../data/body_temperature_data.dart';
 import '../widgets/body_temperature_chart.dart';
 
-
 class BodyTemperatureChartPage extends StatelessWidget {
   const BodyTemperatureChartPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-
     final Object? args = ModalRoute.of(context)!.settings.arguments;
 
     late BodyTemperatureData _bodyTemperatureData;
@@ -22,22 +20,19 @@ class BodyTemperatureChartPage extends StatelessWidget {
     _bodyTemperatureChart.update(bodyTemperatureHistory: _bodyTemperatureData.bodyTemperatureHistory);
 
     return Scaffold(
-        appBar: AppBar(
-          title: const Text("Body Temperature"),
+      appBar: AppBar(
+        title: const Text("Body Temperature"),
+      ),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(36.0),
+          child: Column(
+            children: [
+             Expanded(child: _bodyTemperatureChart),
+            ]
+          )
         ),
-        body: Center(
-          child: Container(
-            child: Padding(
-              padding: const EdgeInsets.all(36.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  SizedBox(height: 600, width: 800, child: _bodyTemperatureChart),
-                ],
-              ),
-            ),
-          ),
-        ));
+      ),
+    );
   }
 }
