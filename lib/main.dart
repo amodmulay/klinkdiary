73c7f2df615fd6df82dfd23d3ffe998a266fa064
Christian Felbermair <christian.felbermair@gmx.de>
2022-02-22 15:46:04 +0100

    * fixed incorrect merge

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-
 import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:klinikdiary/pages/blood_pressure_input_page.dart';
 import 'i18n/i18n.dart';
 import 'pages/body_temperature_detail_page.dart';
 import 'pages/body_temperature_list_page.dart';
@@ -10,7 +10,6 @@ import 'pages/homepage.dart';
 import 'pages/pages.dart';
 import 'pages/login_page.dart';
 import 'theme/klinik_theme.dart';
-
 
 
 void main() {
