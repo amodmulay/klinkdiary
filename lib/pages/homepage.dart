b03bc3e706c751a4f63966e1533667e6a4fe6e55
Amod Mulay <amod.mulay@gmail.com>
2022-02-22 13:15:23 +0100

    Added: blood pressure page. changed blood pressure icon. added reusable components

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
-
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:klinikdiary/components/icon_content.dart';
+import 'package:klinikdiary/components/reusable_card.dart';
 import '../data/body_temperature_record.dart';
 import '../widgets/styles.dart';
 import 'pages.dart';
+import '../theme/constants.dart';
 
 class Homepage extends StatelessWidget {
   const Homepage({Key? key}) : super(key: key);
@@ -17,8 +20,18 @@ class Homepage extends StatelessWidget {
         },
         child: const Text('Body Temperature'));
 
-    final Widget _bloodPreassurePageButton =
-        ElevatedButton(style: Styles.buttonStyle, onPressed: () {}, child: const Text('Blood Pressure'));
+    /* final Widget _bloodPreassurePageButton =
+        ElevatedButton(style: Styles.buttonStyle, onPressed: () {}, child: const Text('Blood Pressure')); */
+
+    final Widget _bloodPreassurePageButton = ReusableCard(
+        color: kActiveCardColor,
+        child: IconContent(
+          label: 'Blood Pressure',
+          icon: FontAwesomeIcons.heart,
+        ),
+        onTap: () {
+          Navigator.pushNamed(context, Pages.bloodPressure);
+        });
 
     return Scaffold(
         appBar: AppBar(title: const Text("Klink Diary"), actions: [
@@ -33,8 +46,7 @@ class Homepage extends StatelessWidget {
                       Text("Made by"),
                       Text(" Amod Mulay"),
                       Text(" Christian Felbermair")
-                    ]
-                );
+                    ]);
               })
         ]),
         drawer: Drawer(
@@ -44,9 +56,13 @@ class Homepage extends StatelessWidget {
                 title: const Text("Body Temperature"),
                 onTap: () {
                   Navigator.pushNamed(context, Pages.bodyTemperatureList,
-                      arguments: BodyTemperatureRecord.now(bodyTemperature: 37.5));
+                      arguments:
+                          BodyTemperatureRecord.now(bodyTemperature: 37.5));
                 }),
-            ListTile(leading: const Icon(Icons.opacity), title: const Text("Blood Pressure"), onTap: () {})
+            ListTile(
+                leading: const Icon(Icons.opacity),
+                title: const Text("Blood Pressure"),
+                onTap: () {})
           ]),
         ),
         body: Center(
@@ -55,9 +71,13 @@ class Homepage extends StatelessWidget {
             child: Padding(
               padding: const EdgeInsets.all(36.0),
               child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
                 mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[_bodyTemperaturePageButton, const SizedBox(height: 10.0), _bloodPreassurePageButton],
+                children: <Widget>[
+                  _bodyTemperaturePageButton,
+                  const SizedBox(height: 10.0),
+                  _bloodPreassurePageButton
+                ],
               ),
             ),
           ),
