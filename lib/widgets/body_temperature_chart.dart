608f12249e5e9df4ac48620547d42fb7448fa3d5
Christian Felbermair <christian.felbermair@dxc.com>
2022-03-28 12:42:32 +0200

    * upgraded fl_chart version to 0.50.0

--- lib/widgets/body_temperature_chart.dart
+++ lib/widgets/body_temperature_chart.dart
@@ -1,13 +1,12 @@
 import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
-import 'package:klinikdiary/data/body_temperature_record.dart';
+import '/widgets/formats.dart';
+import '/data/body_temperature_record.dart';
 
-import 'formats.dart';
-
-int displayDaysInPast = 14;
+const int displayDaysInPast = 21;
+const int bottomTitlesGranularity = 4; // in days
 
 class BodyTemperatureChart extends StatelessWidget {
-
   final List<FlSpot> spots = List<FlSpot>.empty(growable: true);
 
   BodyTemperatureChart({Key? key}) : super(key: key);
@@ -42,14 +41,44 @@ class BodyTemperatureChart extends StatelessWidget {
   LineChartData get _lineChartData => LineChartData(
         lineTouchData: _lineTouchData,
         gridData: _gridData,
-        titlesData: _titlesData,
-        borderData: _borderData,
+        titlesData: FlTitlesData(
+            topTitles: AxisTitles(sideTitles: SideTitles(showTitles: false)),
+            leftTitles: AxisTitles(sideTitles: SideTitles(showTitles: true, reservedSize: 30,
+                getTitlesWidget: _leftTitleWidgetFunction)),
+            rightTitles: AxisTitles(sideTitles: SideTitles(showTitles: false)),
+            bottomTitles: AxisTitles(sideTitles: SideTitles(showTitles: true, reservedSize: 30,
+              getTitlesWidget: _bottomTitleWidgetFunction,
+              interval: 1000.0 * 60 * 60 * 24 * bottomTitlesGranularity, ))
+        ),
         lineBarsData: _lineBarsData,
         minX: DateTime.now().subtract(Duration(days: displayDaysInPast)).millisecondsSinceEpoch.toDouble(),
         maxX: DateTime.now().millisecondsSinceEpoch.toDouble(),
         minY: 35,
         maxY: 42,
       );
+
+  Widget _leftTitleWidgetFunction(double value, TitleMeta meta) {
+    const style = TextStyle(
+      color: Color(0xff7589a2),
+      fontWeight: FontWeight.bold,
+      fontSize: 14,
+    );
+    String text = value.toStringAsFixed(1);
+    return Center(child: Text(text, style: style));
+  }
+
+  Widget _bottomTitleWidgetFunction(double value, TitleMeta meta) {
+    const style = TextStyle(
+      color: Color(0xff7589a2),
+      fontWeight: FontWeight.bold,
+      fontSize: 14,
+    );
+
+    final String text = (value % 100000 > 0) ? "" : Formats.dateFormatDaMo.format(DateTime.fromMillisecondsSinceEpoch(value.toInt()));
+
+    return Center(child: Text(text, style: style));
+  }
+
 
   FlGridData get _gridData => FlGridData(
         show: true,
@@ -66,61 +95,28 @@ class BodyTemperatureChart extends StatelessWidget {
 
   LineTouchData get _lineTouchData => LineTouchData(
         handleBuiltInTouches: true,
-        touchTooltipData: LineTouchTooltipData(
+        touchTooltipData: LineTouchTooltipData(getTooltipItems: _lineTooltipItem,
           tooltipBgColor: Colors.blueGrey.withOpacity(0.8),
         ),
       );
 
-  FlTitlesData get _titlesData => FlTitlesData(
-        bottomTitles: _bottomTitles,
-        rightTitles: SideTitles(showTitles: false),
-        topTitles: SideTitles(showTitles: false),
-        leftTitles: _leftTitles(
-          getTitles: (value) {
-            return '${value.toInt()} °C';
-          },
-        ),
+  List<LineTooltipItem> _lineTooltipItem(List<LineBarSpot> touchedSpots) {
+    return touchedSpots.map((LineBarSpot touchedSpot) {
+      final textStyle = TextStyle(
+        color: touchedSpot.bar.gradient?.colors.first ??
+            touchedSpot.bar.color ??
+            Colors.blueGrey,
+        fontWeight: FontWeight.bold,
+        fontSize: 14,
       );
-
+      return LineTooltipItem(touchedSpot.y.toStringAsFixed(1) + " °C", textStyle);
+    }).toList();
+  }
+  
   List<LineChartBarData> get _lineBarsData => [_lineChartBarData()];
-
-  SideTitles _leftTitles({required GetTitleFunction getTitles}) => SideTitles(
-        getTitles: getTitles,
-        showTitles: true,
-        margin: 8,
-        interval: 1,
-        reservedSize: 40,
-        getTextStyles: (context, value) => const TextStyle(
-          color: Colors.blue,
-          fontSize: 14,
-        ),
-      );
-
-  SideTitles get _bottomTitles => SideTitles(
-        showTitles: true,
-        reservedSize: 40,
-        margin: 10,
-        interval: 4 * 24 * 60 * 60 * 1000,
-        getTextStyles: (context, value) => const TextStyle(
-          color: Colors.blue,
-          fontSize: 16,
-        ),
-        getTitles: (value) => (value % 100000 > 0) ? "" : Formats.dateFormatDaMo.format(DateTime.fromMillisecondsSinceEpoch(value.toInt()))
-      );
-
-  FlBorderData get _borderData => FlBorderData(
-        show: true,
-        border: const Border(
-          bottom: BorderSide(color: Color(0xff4e4965), width: 4),
-          left: BorderSide(color: Colors.transparent),
-          right: BorderSide(color: Colors.transparent),
-          top: BorderSide(color: Colors.transparent),
-        ),
-      );
 
   LineChartBarData _lineChartBarData() {
     return LineChartBarData(
-      colors: [Colors.blue],
       isCurved: true,
       barWidth: 4,
       isStrokeCapRound: true,
@@ -129,5 +125,4 @@ class BodyTemperatureChart extends StatelessWidget {
       spots: spots,
     );
   }
-
 }
